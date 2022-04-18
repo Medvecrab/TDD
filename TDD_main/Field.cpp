63f4509c3b9ded467b9ed1f72da6952daec841a9
Medvecab <mr.medvecrab@gmail.com>
2022-04-18 16:27:42 +0700

    Функция исправлена, первый тест пройден

--- TDD_main/Field.cpp
+++ TDD_main/Field.cpp
@@ -5,16 +5,17 @@ void Field::fill_default()
 	char current_symbol = '~'; //для пустого места
 	for (int i = 0; i < size + PADDING; i++)
 	{
-		if (i == 0)
-			current_symbol = 'A';
 		for (int j = 0; j < size + PADDING; j++)
 		{
+			if (i == 0)
+				current_symbol = 'A';
 			if (j == 0)
 				current_symbol = '0';
 			if (i == 0 && j == 0)
 				current_symbol = ' ';
 			//automatic symbol calculation
-			field[i][j] = current_symbol + (j == 0 && i > 0 ? i : 0) + (i == 0 && j > 0 ? j : 0);
+			field[i][j] = current_symbol + (j == 0 && i > 1 ? i - 1 : 0) + (i == 0 && j > 1 ? j - 1 : 0);
+			current_symbol = '~';
 		}
 	}
 	return;
